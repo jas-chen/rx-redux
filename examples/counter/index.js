1c995d561d09629a68a243f069a86c3bfeab3684
Jas Chen <jas228@gmail.com>
2015-07-23 17:14:26 +0800

    more ES2015 style

--- examples/counter/index.js
+++ examples/counter/index.js
@@ -5,32 +5,27 @@ import * as CounterActions from './actions/CounterActions';
 
 const counter = document.getElementById('count');
 
-const decreaseAction$ = (function() {
+const decreaseAction$ = (() => {
     const decrease = document.getElementById('btn-decrease');
     const event$ = Rx.Observable.fromEvent(decrease, 'click');
     return event$.map(CounterActions.decrement);
-}());
+})();
 
-const increaseAction$ = (function() {
+const increaseAction$ = (() => {
     const increase = document.getElementById('btn-increase');
     const event$ = Rx.Observable.fromEvent(increase, 'click');
     return event$.map(CounterActions.increment);
-}());
+})();
 
 const initAction = {};
 const action$ = Rx.Observable.merge(decreaseAction$, increaseAction$).startWith(initAction);
 const {state$, start} = rxRedux(action$, reducers);
 
 state$.subscribe(
-    function (count) {
+    count => {
         console.log('update UI');
         counter.textContent = count;
-    },
-    function (err) {
-        console.log('state$ error: ', err.stack);
-    },
-    function () {
-        console.log('state$ completed');
-    });
+    }
+);
 
 start();
--- src/index.js
+++ src/index.js
@@ -3,50 +3,44 @@ export default function(action$, reducers, initState){
 
     const nextState$ = new Rx.Subject();
 
-    const currentState$ = Rx.Observable.create(function(observer){
+    const currentState$ = Rx.Observable.create(observer => {
         console.log('init state:', initState);
         observer.onNext(initState);
 
         nextState$.subscribe(
-            function (state) {
+            state => {
                 console.log('set current state to:', state);
                 observer.onNext(state);
             },
-            function (err) {
-                console.error('subject error: ', err.stacktrace);
-            },
-            function () {
-                console.log('subject completed');
-            });
+            err => console.error('subject error: ', err.stacktrace),
+            () => console.log('subject completed')
+        );
     });
 
     function start() {
         console.log('start');
 
-        const dispatcher$ = action$.withLatestFrom(currentState$, function(action, state){
+        const dispatcher$ = action$.withLatestFrom(currentState$, (action, state) => {
             console.log('dispatcher get action:', action.type, ', current state:', state);
-            reducers.forEach(function(reducer){
+            reducers.forEach(reducer => {
                 state = reducer(state, action);
             });
 
-            return {action: action, state: state};
+            return {action, state}
         });
 
         dispatcher$.subscribe(
-            function (result) {
+            result => {
                 console.log('action:', result.action, 'completed, change state to:', result.state);
                 nextState$.onNext(result.state);
             },
-            function (err) {
-                console.error('dispatcher$ error:', err.stacktrace);
-            },
-            function () {
-                console.log('dispatcher$ completed');
-            });
+            err => console.error('dispatcher$ error:', err.stacktrace),
+            () => console.log('dispatcher$ completed')
+        );
     }
 
     return {
-        start: start,
+        start,
         state$: nextState$
     }
 }
