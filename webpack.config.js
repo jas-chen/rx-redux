8a09eb550001a879b8e22041d5ba9a486f7d6099
Jas Chen <jas228@gmail.com>
2015-07-27 16:13:14 +0800

    clean up

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,36 @@
 var webpack = require('webpack');
 
 var plugins = [
-    new webpack.DefinePlugin({
-        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    }),
-    new webpack.optimize.OccurenceOrderPlugin()
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  }),
+  new webpack.optimize.OccurenceOrderPlugin()
 ];
 
 if (process.env.NODE_ENV === 'production') {
-    plugins.push(
-        new webpack.optimize.UglifyJsPlugin({
-            compressor: {
-                screw_ie8: true,
-                warnings: false
-            }
-        })
-    );
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        screw_ie8: true,
+        warnings: false
+      }
+    })
+  );
 }
 
 module.exports = {
-    entry: './src/index',
-    output: {
-        library: 'RxRedux',
-        libraryTarget: 'umd'
-    },
-    module: {
-        loaders: [
-            { test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/ }
-        ]
-    },
-    plugins: plugins,
-    resolve: {
-        extensions: ['', '.js']
-    }
+  entry: './src/index',
+  output: {
+    library: 'RxRedux',
+    libraryTarget: 'umd'
+  },
+  module: {
+    loaders: [
+      {test: /\.js$/, loaders: ['babel-loader'], exclude: /node_modules/}
+    ]
+  },
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.js']
+  }
 };
