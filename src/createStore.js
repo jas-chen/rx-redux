2350773ad65230e863659cf76e289c1523ab63a3
Jas Chen <jas228@gmail.com>
2015-07-27 11:36:39 +0800

    v0.3.3
    refactor createStore

--- src/createStore.js
+++ src/createStore.js
@@ -1,11 +1,24 @@
 import Rx from 'rx'
 import isPlainObject from './utils/isPlainObject'
 
-const initAction = {type: '@@rx-redux/INIT_' + Math.random()};
+function createDispatcher() {
+    const dispatcher$ = new Rx.Subject();
 
-function createDispatch(reducer) {
-    let state;
+    dispatcher$.subscribeOnCompleted(() => {
+        console.warn('[rx-redux] dispatcher$ stream completed.');
+    });
+
+    return dispatcher$;
+}
+
+export default function createStore(reducer, initState) {
+    const initAction = {type: '@@rx-redux/INIT_' + Math.random()};
     const listeners = [];
+
+    const dispatcher$ = createDispatcher();
+    const state$ = dispatcher$.map(dispatch).do(() => listeners.forEach(listener => listener()));
+
+    let state = reducer(initState, initAction);
 
     function dispatch(action) {
         if(!isPlainObject(action)) {
@@ -13,47 +26,28 @@ function createDispatch(reducer) {
         }
         else {
             state = reducer(state, action);
-            listeners.forEach(listener => listener());
         }
 
         return state
     }
 
     function subscribe(listener) {
+        // this doesn't work, so sad.
+        // state$.do(listener);
+
         listeners.push(listener);
         return () => listeners.splice(listeners.indexOf(listener), 1)
     }
 
     function replaceReducer(newReducer) {
         reducer = newReducer;
-        console.log('new reducer:', reducer);
         dispatch(initAction);
     }
 
     return {
-        dispatch,
-        subscribe,
-        getState: () => state,
-        setState: (newState) => { state = newState; },
-        replaceReducer
-    }
-}
-
-export default function createStore(reducer, initState) {
-    const {dispatch, getState, subscribe, setState, replaceReducer} = createDispatch(reducer);
-    setState(reducer(initState, initAction));
-    const dispatcher$ = new Rx.Subject();
-
-    dispatcher$.subscribeOnCompleted(() => {
-        console.warn('[rx-redux] dispatcher$ stream completed.');
-    });
-
-    const state$ = dispatcher$.map(dispatch);
-
-    return {
         state$,
         dispatcher$,
-        getState,
+        getState: () => state,
         dispatch: dispatcher$.onNext.bind(dispatcher$),
         subscribe,
         getReducer: () => reducer,
