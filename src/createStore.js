0fe887e646d1507cb49f8df24cc88b91f5822f68
Jas Chen <jas228@gmail.com>
2015-07-27 19:48:59 +0800

    refactor

--- src/createStore.js
+++ src/createStore.js
@@ -3,27 +3,15 @@ import isPlainObject from './utils/isPlainObject';
 
 export default function createStore(reducer, initState) {
   const initAction = {type: '@@rx-redux/INIT_' + (new Date()).getTime()};
-  let currentReducer = reducer;
   const listeners = [];
+  const dispatcher$ = new Rx.Subject();
+
+  let currentReducer = reducer;
+  let state = currentReducer(initState, initAction);
 
   function callListeners() {
     listeners.forEach(listener => listener());
   }
-
-  let state = currentReducer(initState, initAction);
-
-  function reduce(action) {
-    if (!isPlainObject(action)) {
-      throw new Error('[reducer] Action must be a plain object. Current state will be returned.');
-    } else {
-      state = currentReducer(state, action);
-    }
-
-    return state;
-  }
-
-  const dispatcher$ = new Rx.Subject();
-  const state$ = dispatcher$.map(reduce).do(callListeners);
 
   function dispatch(action) {
     dispatcher$.onNext(action);
@@ -39,6 +27,18 @@ export default function createStore(reducer, initState) {
     currentReducer = newReducer;
     dispatch(initAction);
   }
+
+  function reduce(action) {
+    if (!isPlainObject(action)) {
+      throw new Error('[reducer] Action must be a plain object. Current state will be returned.');
+    } else {
+      state = currentReducer(state, action);
+    }
+
+    return state;
+  }
+
+  const state$ = dispatcher$.map(reduce).do(callListeners);
 
   return {
     state$,
