b5b8ba033f9e07678954336c2ce41adecd7ef361
Jas Chen <jas228@gmail.com>
2015-07-31 17:04:13 +0800

    let connectAction send error to store

--- src/utils/connectAction.js
+++ src/utils/connectAction.js
@@ -4,7 +4,7 @@ export default function connectAction(action$, store) {
       store.dispatch(action)
     },
     (err) => {
-      throw new Error(err);
+      store.dispatcher$.onError(err);
     },
     () => {
       store.dispatcher$.onCompleted();
