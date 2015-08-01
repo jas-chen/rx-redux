0f8e7d7c40e3e886fdd15a1e83f9071c9106cc45
Jas Chen <jas228@gmail.com>
2015-08-01 12:04:20 +0800

    v0.5.7
    improve code readability

--- src/createStore.js
+++ src/createStore.js
@@ -2,6 +2,10 @@ import Rx from 'rx';
 import isPlainObject from './utils/isPlainObject';
 
 export default function createStore(reducer, initState) {
+  if(typeof reducer !== 'function') {
+    throw new Error('Expected the reducer to be a function.');
+  }
+
   const initAction = {type: '@@rx-redux/INIT_' + (new Date()).getTime()};
   const listeners = [];
   const dispatcher$ = new Rx.Subject();
@@ -20,25 +24,31 @@ export default function createStore(reducer, initState) {
 
   function subscribe(listener) {
     listeners.push(listener);
-    return () => listeners.splice(listeners.indexOf(listener), 1);
+
+    return function unsubscribe() {
+      var index = listeners.indexOf(listener);
+      listeners.splice(index, 1);
+    };
   }
 
   function replaceReducer(newReducer) {
     currentReducer = newReducer;
-    dispatch(initAction);
+    dispatcher$.onNext(initAction);
   }
 
   function reduce(action) {
     if (!isPlainObject(action)) {
-      throw new Error('Action must be a plain object. Current state will be returned.');
-    } else {
-      state = currentReducer(state, action);
+      throw new Error('Actions must be plain objects.');
     }
+
+    state = currentReducer(state, action);
 
     return state;
   }
 
   const state$ = dispatcher$.map(reduce).publish().refCount().startWith(state);
+
+  // must call state$.subscribe() to start life cycle
   state$.subscribe(
     callListeners,
     err => { throw err; }
